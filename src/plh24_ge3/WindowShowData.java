16a5e01e0ee7627e64eb27d38e399e062ba4bc5d
ody1 <ody1@users.noreply.github.com>
2022-02-11 09:02:23 +0200

    WindowManageData, WindowShowData, WindowShowStats: Refactor code, Added game selection

--- src/plh24_ge3/WindowShowData.java
+++ src/plh24_ge3/WindowShowData.java
@@ -3,12 +3,14 @@ package plh24_ge3;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.GridLayout;
 import javax.swing.BorderFactory;
 import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -46,56 +48,91 @@ public class WindowShowData
 		Color backColor = new java.awt.Color(244, 244, 250);
 
 
-		// Labels
-		JLabel labelTitle = new JLabel("Προβολή δεδομένων");
-		labelTitle.setFont(new Font("Arial", 3, 42));
-		labelTitle.setForeground(Color.ORANGE);
-
-		JLabel labelTitleShadow = new JLabel("Προβολή δεδομένων");
-		labelTitleShadow.setFont(new Font("Arial", 3, 42));
-		labelTitleShadow.setForeground(Color.BLUE);
-
-
-
-		// Buttons
-		JButton buttonClose = new JButton("Κλείσιμο");
-		buttonClose.setPreferredSize(new java.awt.Dimension(116, 26));
-        buttonClose.addActionListener(this::buttonCloseActionPerformed);
-
-
-
-		// Panels
-		JPanel topPanel = new JPanel();    // Panel with the window title label
+		/*
+		 * Top panel with the window title
+		 */
+		JPanel topPanel = new JPanel();
 		topPanel.setBorder(BorderFactory.createEmptyBorder(6, 30, 0, 0));
 		topPanel.setLayout(new GridLayout(0, 1, 0, -76));
 		topPanel.setPreferredSize(new java.awt.Dimension(490, 89));
 		topPanel.setMinimumSize(new java.awt.Dimension(490, 89));
 		topPanel.setMaximumSize(new java.awt.Dimension(490, 89));
 		topPanel.setBackground(backColor);
+
+			// Labels with the title
+			JLabel labelTitle = new JLabel("Προβολή δεδομένων");
+			labelTitle.setFont(new Font("Arial", 3, 42));
+			labelTitle.setForeground(Color.ORANGE);
+
+			JLabel labelTitleShadow = new JLabel("Προβολή δεδομένων");
+			labelTitleShadow.setFont(new Font("Arial", 3, 42));
+			labelTitleShadow.setForeground(Color.BLUE);
+
 		topPanel.add(labelTitle);
 		topPanel.add(labelTitleShadow);
 
 
+		/*
+		 * Middle panel with all the functionality
+		 */
+		JPanel middlePanel = new JPanel();
+		middlePanel.setBorder(BorderFactory.createEmptyBorder(0, 20, 0, 20));
+		middlePanel.setLayout(new BoxLayout(middlePanel, BoxLayout.X_AXIS));
+		middlePanel.setBackground(backColor);
+
+			// Game selection panel
+			JPanel gameSelectPanel = new JPanel();
+			gameSelectPanel.setBorder(BorderFactory.createEmptyBorder(0, 0, 0, 0));
+			gameSelectPanel.setLayout(new FlowLayout(0, 0, 0));  // align,hgap,vgap (1,5,5)
+			gameSelectPanel.setBackground(backColor);
+
+				JLabel labelGameSelect = new JLabel("Επιλέξτε τυχερό παιχνίδι");
+
+				String comboBoxGameSelectItems[] = {"Τζόκερ (id: 5104)"};
+				JComboBox comboBoxGameSelect = new JComboBox(comboBoxGameSelectItems);
+				comboBoxGameSelect.setBorder(BorderFactory.createEmptyBorder(0, 10, 0, 0));
+				comboBoxGameSelect.setBackground(backColor);
+
+			gameSelectPanel.add(labelGameSelect);
+			gameSelectPanel.add(comboBoxGameSelect);
+
+		middlePanel.add(gameSelectPanel, BorderLayout.CENTER);
+		middlePanel.add(Box.createVerticalGlue(), BorderLayout.CENTER);
 
 
-
-		JPanel bottomPanel = new JPanel();    // Panel with Close button
+		/*
+		 * Bottom panel with the Close button
+		 */
+		JPanel bottomPanel = new JPanel();
 		bottomPanel.setBorder(BorderFactory.createEmptyBorder(20, 20, 0, 20));
 		bottomPanel.setLayout(new BoxLayout(bottomPanel, BoxLayout.X_AXIS));
 		bottomPanel.setBackground(backColor);
+
+			// Close button
+			JButton buttonClose = new JButton("Κλείσιμο");
+			buttonClose.setPreferredSize(new java.awt.Dimension(116, 26));
+			buttonClose.addActionListener(this::buttonCloseActionPerformed);
+
 		bottomPanel.add(Box.createHorizontalGlue(), BorderLayout.CENTER);
 		bottomPanel.add(buttonClose);
 
-		JPanel mainPanel = new JPanel();    // Main panel
+
+		/*
+		 * Main panel
+		 */
+		JPanel mainPanel = new JPanel();
 		mainPanel.setBorder(BorderFactory.createEmptyBorder(0, 0, 30, 0));
 		mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
 		mainPanel.setPreferredSize(new java.awt.Dimension(596, 362));
 		mainPanel.setBackground(backColor);
 		mainPanel.add(topPanel);
+		mainPanel.add(middlePanel);
 		mainPanel.add(bottomPanel);
 
 
-		// Main window
+		/*
+		 * Main window
+		 */
 		dialog = new JDialog();
 		dialog.add(mainPanel, BorderLayout.CENTER);
 		dialog.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
@@ -103,7 +140,8 @@ public class WindowShowData
 		dialog.setModal(true);
 		dialog.pack();
 		dialog.setLocationRelativeTo(null);   // Appear in the center of screen
-		dialog.setResizable(false);
+		dialog.setMinimumSize(new java.awt.Dimension(590, 360));
+//		dialog.setResizable(false);
 		dialog.setVisible(true);
 	}
 }

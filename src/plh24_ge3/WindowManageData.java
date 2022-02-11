3207b516abcd034517b2c6b8fa52b056ba99a6d2
ody1 <ody1@users.noreply.github.com>
2022-02-11 17:32:13 +0200

    WindowManageData update

--- src/plh24_ge3/WindowManageData.java
+++ src/plh24_ge3/WindowManageData.java
@@ -3,18 +3,22 @@ package plh24_ge3;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.GridLayout;
 import javax.swing.BorderFactory;
 import javax.swing.Box;
 import javax.swing.BoxLayout;
+import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.JTextField;
 
 
 /**
@@ -29,16 +33,21 @@ public class WindowManageData
 	private final JDialog dialog;
 
 
-
-
 	// Button actions
+	private void buttonFindLatestDrawActionPerformed(java.awt.event.ActionEvent evt)
+	{
+		// TODO
+	}
+
+	private void buttonDownloadActionPerformed(java.awt.event.ActionEvent evt)
+	{
+		// TODO
+	}
+
 	private void buttonCloseActionPerformed(java.awt.event.ActionEvent evt)
 	{
 		dialog.dispose();
 	}
-
-
-
 
 
 	// Constructor
@@ -54,9 +63,9 @@ public class WindowManageData
 		JPanel topPanel = new JPanel();
 		topPanel.setBorder(BorderFactory.createEmptyBorder(6, 30, 0, 0));
 		topPanel.setLayout(new GridLayout(0, 1, 0, -76));
-		topPanel.setPreferredSize(new java.awt.Dimension(514, 89));
-		topPanel.setMinimumSize(new java.awt.Dimension(514, 89));
-		topPanel.setMaximumSize(new java.awt.Dimension(514, 89));
+		topPanel.setPreferredSize(new Dimension(514, 89));
+		topPanel.setMinimumSize(new Dimension(514, 89));
+		topPanel.setMaximumSize(new Dimension(514, 89));
 		topPanel.setBackground(backColor);
 
 			// Labels with the title
@@ -77,17 +86,20 @@ public class WindowManageData
 		 */
 		JPanel middlePanel = new JPanel();
 		middlePanel.setBorder(BorderFactory.createEmptyBorder(0, 20, 0, 20));
-		middlePanel.setLayout(new BoxLayout(middlePanel, BoxLayout.X_AXIS));
+		middlePanel.setLayout(new BoxLayout(middlePanel, BoxLayout.Y_AXIS));
 		middlePanel.setBackground(backColor);
 
 			// Game selection panel
 			JPanel gameSelectPanel = new JPanel();
 			gameSelectPanel.setBorder(BorderFactory.createEmptyBorder(0, 0, 0, 0));
 			gameSelectPanel.setLayout(new FlowLayout(0, 0, 0));  // align,hgap,vgap (1,5,5)
+			gameSelectPanel.setMaximumSize(new Dimension(Integer.MAX_VALUE, gameSelectPanel.getMinimumSize().height));
 			gameSelectPanel.setBackground(backColor);
 
+				// Label game select
 				JLabel labelGameSelect = new JLabel("Επιλέξτε τυχερό παιχνίδι");
 
+				// ComboBox game select
 				String comboBoxGameSelectItems[] = {"Τζόκερ (id: 5104)"};
 				JComboBox comboBoxGameSelect = new JComboBox(comboBoxGameSelectItems);
 				comboBoxGameSelect.setBorder(BorderFactory.createEmptyBorder(0, 10, 0, 0));
@@ -96,8 +108,131 @@ public class WindowManageData
 			gameSelectPanel.add(labelGameSelect);
 			gameSelectPanel.add(comboBoxGameSelect);
 
-		middlePanel.add(gameSelectPanel, BorderLayout.CENTER);
-		middlePanel.add(Box.createVerticalGlue(), BorderLayout.CENTER);
+
+			// Choose search method label panel
+			JPanel chooseMethodLabelPanel = new JPanel();
+			chooseMethodLabelPanel.setBorder(BorderFactory.createEmptyBorder(12, 0, 0, 0));
+			chooseMethodLabelPanel.setLayout(new FlowLayout(0, 0, 0));
+			chooseMethodLabelPanel.setMaximumSize(new Dimension(Integer.MAX_VALUE, chooseMethodLabelPanel.getMinimumSize().height));
+			chooseMethodLabelPanel.setBackground(backColor);
+
+				// Label choose method
+				JLabel labelChooseMethod = new JLabel("Επιλέξτε τρόπο αναζήτησης κληρώσεων");
+
+			chooseMethodLabelPanel.add(labelChooseMethod);
+
+
+			// Single draw method panel
+			JPanel singleDrawMethodPanel = new JPanel();
+			singleDrawMethodPanel.setBorder(BorderFactory.createEmptyBorder(6, 0, 0, 0));
+			singleDrawMethodPanel.setLayout(new FlowLayout(0, 0, 0));
+			singleDrawMethodPanel.setMaximumSize(new Dimension(Integer.MAX_VALUE, singleDrawMethodPanel.getMinimumSize().height));
+			singleDrawMethodPanel.setBackground(backColor);
+
+				// Radio button for single draw
+				JRadioButton radioButtonSingleDraw = new JRadioButton();
+				radioButtonSingleDraw.setText("Συγκεκριμένη κλήρωση");
+				radioButtonSingleDraw.setBorder(BorderFactory.createEmptyBorder(0, 10, 0, 0));
+				radioButtonSingleDraw.setBackground(backColor);
+				radioButtonSingleDraw.setSelected(true);
+
+				// Label draw id
+				JLabel labelDrawId = new JLabel("Αριθμός κλήρωσης");
+				labelDrawId.setBorder(BorderFactory.createEmptyBorder(0, 30, 0, 6));
+
+				// Text field for draw id
+				JTextField textFieldDrawId = new JTextField();
+				textFieldDrawId.setBorder(BorderFactory.createEmptyBorder(0, 4, 0, 4));
+				textFieldDrawId.setPreferredSize(new Dimension(38, 20));
+
+				// Button to find latest draw
+				JButton buttonFindLatestDraw = new JButton("Εύρεση τελευταίας κλήρωσης");
+				buttonFindLatestDraw.setPreferredSize(new Dimension(206, 20));
+				buttonFindLatestDraw.addActionListener(this::buttonFindLatestDrawActionPerformed);
+
+			singleDrawMethodPanel.add(radioButtonSingleDraw);
+			singleDrawMethodPanel.add(labelDrawId);
+			singleDrawMethodPanel.add(textFieldDrawId);
+			singleDrawMethodPanel.add(Box.createRigidArea(new Dimension(99,0)));
+			singleDrawMethodPanel.add(buttonFindLatestDraw);
+
+
+			// Date range method panel
+			JPanel dateRangeMethodPanel = new JPanel();
+			dateRangeMethodPanel.setBorder(BorderFactory.createEmptyBorder(6, 0, 0, 0));
+			dateRangeMethodPanel.setLayout(new FlowLayout(0, 0, 0));
+			dateRangeMethodPanel.setMaximumSize(new Dimension(Integer.MAX_VALUE, dateRangeMethodPanel.getMinimumSize().height));
+			dateRangeMethodPanel.setBackground(backColor);
+
+				// Radio button for date range
+				JRadioButton radioButtonDateRange = new JRadioButton();
+				radioButtonDateRange.setText("Εύρος ημερομηνιών");
+				radioButtonDateRange.setBorder(BorderFactory.createEmptyBorder(0, 10, 0, 0));
+				radioButtonDateRange.setBackground(backColor);
+
+				// Group radio butttons
+				ButtonGroup groupChooseMethod = new ButtonGroup();
+				groupChooseMethod.add(radioButtonSingleDraw);
+				groupChooseMethod.add(radioButtonDateRange);
+
+				// Label from
+				JLabel labelFrom = new JLabel("Από");
+				labelFrom.setBorder(BorderFactory.createEmptyBorder(0, 53, 0, 6));
+
+				// Text field for date 1
+				JTextField textFieldDate1 = new JTextField();
+				textFieldDate1.setBorder(BorderFactory.createEmptyBorder(0, 4, 0, 4));
+				textFieldDate1.setPreferredSize(new Dimension(74, 20));
+
+				// Label up to
+				JLabel labelUpTo = new JLabel("Έως");
+				labelUpTo.setBorder(BorderFactory.createEmptyBorder(0, 8, 0, 6));
+
+				// Text field for date 2
+				JTextField textFieldDate2 = new JTextField();
+				textFieldDate2.setBorder(BorderFactory.createEmptyBorder(0, 4, 0, 4));
+				textFieldDate2.setPreferredSize(new Dimension(74, 20));
+
+				// Label predefined date range
+				JLabel labelPredefinedRange = new JLabel("Προκαθορισμένες επιλογές");
+				labelPredefinedRange.setBorder(BorderFactory.createEmptyBorder(0, 30, 0, 6));
+
+				// ComboBox quick select
+				String comboBoxPredefinedRangeItems[] = {"Τελευταία εβδομάδα", "Τελευταίος μήνας", "Τελευταίο 3μηνο", "Τελευταίο έτος"};
+				JComboBox comboBoxPredefinedRange = new JComboBox(comboBoxPredefinedRangeItems);
+				comboBoxPredefinedRange.setBorder(BorderFactory.createEmptyBorder(0, 4, 0, 0));
+				comboBoxPredefinedRange.setPreferredSize(new Dimension(152, 20));
+				comboBoxPredefinedRange.setBackground(backColor);
+
+			dateRangeMethodPanel.add(radioButtonDateRange);
+			dateRangeMethodPanel.add(labelFrom);
+			dateRangeMethodPanel.add(textFieldDate1);
+			dateRangeMethodPanel.add(labelUpTo);
+			dateRangeMethodPanel.add(textFieldDate2);
+			dateRangeMethodPanel.add(labelPredefinedRange);
+			dateRangeMethodPanel.add(comboBoxPredefinedRange);
+
+
+			// Download panel
+			JPanel downloadPanel = new JPanel();
+			downloadPanel.setBorder(BorderFactory.createEmptyBorder(18, 0, 0, 0));
+			downloadPanel.setLayout(new FlowLayout(0, 0, 0));  // align,hgap,vgap (1,5,5)
+			downloadPanel.setMaximumSize(new Dimension(Integer.MAX_VALUE, gameSelectPanel.getMinimumSize().height));
+			downloadPanel.setBackground(backColor);
+
+
+				// Button download
+				JButton buttonDownload = new JButton("Αναζήτηση και προβολή δεδομένων");
+				buttonDownload.addActionListener(this::buttonDownloadActionPerformed);
+
+			downloadPanel.add(buttonDownload);
+
+		middlePanel.add(gameSelectPanel);
+		middlePanel.add(chooseMethodLabelPanel);
+		middlePanel.add(singleDrawMethodPanel);
+		middlePanel.add(dateRangeMethodPanel);
+		middlePanel.add(downloadPanel);
+		middlePanel.add(Box.createVerticalGlue());
 
 
 		/*
@@ -110,7 +245,7 @@ public class WindowManageData
 
 			// Close button
 			JButton buttonClose = new JButton("Κλείσιμο");
-			buttonClose.setPreferredSize(new java.awt.Dimension(116, 26));
+			buttonClose.setPreferredSize(new Dimension(116, 26));
 			buttonClose.addActionListener(this::buttonCloseActionPerformed);
 
 		bottomPanel.add(Box.createHorizontalGlue(), BorderLayout.CENTER);
@@ -123,7 +258,7 @@ public class WindowManageData
 		JPanel mainPanel = new JPanel();
 		mainPanel.setBorder(BorderFactory.createEmptyBorder(0, 0, 30, 0));
 		mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
-		mainPanel.setPreferredSize(new java.awt.Dimension(596, 362));
+		mainPanel.setPreferredSize(new Dimension(800, 482));
 		mainPanel.setBackground(backColor);
 		mainPanel.add(topPanel);
 		mainPanel.add(middlePanel);
@@ -140,8 +275,7 @@ public class WindowManageData
 		dialog.setModal(true);
 		dialog.pack();
 		dialog.setLocationRelativeTo(null);   // Appear in the center of screen
-		dialog.setMinimumSize(new java.awt.Dimension(590, 360));
-//		dialog.setResizable(false);
+		dialog.setMinimumSize(new Dimension(800, 486));
 		dialog.setVisible(true);
 	}
 }

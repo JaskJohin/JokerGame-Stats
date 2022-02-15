b35653e9d69f92705718a1eb478fa71ccd768c1e
ody1 <ody1@users.noreply.github.com>
2022-02-15 09:16:11 +0200

    JokerDrawData: Added columns property & changed all dividend to double

--- src/plh24_ge3/JokerDrawData.java
+++ src/plh24_ge3/JokerDrawData.java
@@ -3,8 +3,8 @@ package plh24_ge3;
 /**
  * Data of a single draw of the game Joker.
  * <pre>Constructor:
- * JokerDrawData(int drawId, String drawDate, int winningNum1, int winningNum2,
- *     int winningNum3, int winningNum4, int winningNum5, int bonusNum,
+ * JokerDrawData(int drawId, String drawDate, int columns, int winningNum1,
+ *     int winningNum2, int winningNum3, int winningNum4, int winningNum5, int bonusNum,
  *     int prizeTier5_1winners, int prizeTier5_1dividend,
  *     int prizeTier5winners,   int prizeTier5dividend,
  *     int prizeTier4_1winners, int prizeTier4_1dividend,
@@ -16,6 +16,7 @@ package plh24_ge3;
  * <pre>Methods:
  * getDrawId()                  Get the drawId.
  * getDrawDate()                Get the drawDate.
+ * getColumns()                 Get the number of columns.
  * getWinningNum1()             Get the 1st winning number.
  * getWinningNum2()             Get the 2nd winning number.
  * getWinningNum3()             Get the 3rd winning number.
@@ -50,6 +51,7 @@ public class JokerDrawData
 	// Attributes
 	private final int drawId;
 	private final String drawDate;
+	private final int columns;
 	private final int winningNum1;
 	private final int winningNum2;
 	private final int winningNum3;
@@ -57,27 +59,28 @@ public class JokerDrawData
 	private final int winningNum5;
 	private final int bonusNum;
 	private final int prizeTier5_1winners;
-	private final int prizeTier5_1dividend;
+	private final double prizeTier5_1dividend;
 	private final int prizeTier5winners;
-	private final int prizeTier5dividend;
+	private final double prizeTier5dividend;
 	private final int prizeTier4_1winners;
-	private final int prizeTier4_1dividend;
+	private final double prizeTier4_1dividend;
 	private final int prizeTier4winners;
-	private final int prizeTier4dividend;
+	private final double prizeTier4dividend;
 	private final int prizeTier3_1winners;
-	private final int prizeTier3_1dividend;
+	private final double prizeTier3_1dividend;
 	private final int prizeTier3winners;
-	private final int prizeTier3dividend;
+	private final double prizeTier3dividend;
 	private final int prizeTier2_1winners;
-	private final int prizeTier2_1dividend;
+	private final double prizeTier2_1dividend;
 	private final int prizeTier1_1winners;
-	private final int prizeTier1_1dividend;
+	private final double prizeTier1_1dividend;
 
 
 	// Constructor
 	/**
 	 * @param drawId                The draw id.
 	 * @param drawDate              The draw date.
+	 * @param columns               The number of columns.
 	 * @param winningNum1           The 1st winning number.
 	 * @param winningNum2           The 2nd winning number.
 	 * @param winningNum3           The 3rd winning number.
@@ -101,19 +104,20 @@ public class JokerDrawData
 	 * @param prizeTier1_1winners   The number of winners in the "1+1" tier.
 	 * @param prizeTier1_1dividend  The shares of each winner in the "1+1" tier.
 	 */
-	public JokerDrawData(int drawId, String drawDate, int winningNum1, int winningNum2,
-		int winningNum3, int winningNum4, int winningNum5, int bonusNum,
-		int prizeTier5_1winners, int prizeTier5_1dividend,
-		int prizeTier5winners,   int prizeTier5dividend,
-		int prizeTier4_1winners, int prizeTier4_1dividend,
-		int prizeTier4winners,   int prizeTier4dividend,
-		int prizeTier3_1winners, int prizeTier3_1dividend,
-		int prizeTier3winners,   int prizeTier3dividend,
-		int prizeTier2_1winners, int prizeTier2_1dividend,
-		int prizeTier1_1winners, int prizeTier1_1dividend)
+	public JokerDrawData(int drawId, String drawDate, int columns, int winningNum1,
+		int winningNum2, int winningNum3, int winningNum4, int winningNum5, int bonusNum,
+		int prizeTier5_1winners, double prizeTier5_1dividend,
+		int prizeTier5winners,   double prizeTier5dividend,
+		int prizeTier4_1winners, double prizeTier4_1dividend,
+		int prizeTier4winners,   double prizeTier4dividend,
+		int prizeTier3_1winners, double prizeTier3_1dividend,
+		int prizeTier3winners,   double prizeTier3dividend,
+		int prizeTier2_1winners, double prizeTier2_1dividend,
+		int prizeTier1_1winners, double prizeTier1_1dividend)
 	{
 		this.drawId = drawId;
 		this.drawDate = drawDate;
+		this.columns = columns;
 		this.winningNum1 = winningNum1;
 		this.winningNum2 = winningNum2;
 		this.winningNum3 = winningNum3;
@@ -151,6 +155,12 @@ public class JokerDrawData
 	 * @return A string representing the draw date.
 	 */
 	public String getDrawDate() {return this.drawDate;}
+
+	/**
+	 * Get the number of columns.
+	 * @return An int representing the number of columns.
+	 */
+	public int getColumns() {return this.columns;}
 
 	/**
 	 * Get the 1st winning number.
@@ -198,7 +208,7 @@ public class JokerDrawData
 	 * Get the shares of each winner in the "5+1" tier.
 	 * @return An int representing the shares of each winner in the "5+1" tier.
 	 */
-	public int getPrizeTier5_1dividend() {return this.prizeTier5_1dividend;}
+	public double getPrizeTier5_1dividend() {return this.prizeTier5_1dividend;}
 
 	/**
 	 * Get the number of winners in the "5" tier.
@@ -210,7 +220,7 @@ public class JokerDrawData
 	 * Get the shares of each winner in the "5" tier.
 	 * @return An int representing the shares of each winner in the "5" tier.
 	 */
-	public int getPrizeTier5dividend() {return this.prizeTier5dividend;}
+	public double getPrizeTier5dividend() {return this.prizeTier5dividend;}
 
 	/**
 	 * Get the number of winners in the "4+1" tier.
@@ -222,7 +232,7 @@ public class JokerDrawData
 	 * Get the shares of each winner in the "4+1" tier.
 	 * @return An int representing the shares of each winner in the "4+1" tier.
 	 */
-	public int getPrizeTier4_1dividend() {return this.prizeTier4_1dividend;}
+	public double getPrizeTier4_1dividend() {return this.prizeTier4_1dividend;}
 
 	/**
 	 * Get the number of winners in the "4" tier.
@@ -234,7 +244,7 @@ public class JokerDrawData
 	 * Get the shares of each winner in the "4" tier.
 	 * @return An int representing the shares of each winner in the "4" tier.
 	 */
-	public int getPrizeTier4dividend() {return this.prizeTier4dividend;}
+	public double getPrizeTier4dividend() {return this.prizeTier4dividend;}
 
 	/**
 	 * Get the number of winners in the "3+1" tier.
@@ -246,7 +256,7 @@ public class JokerDrawData
 	 * Get the shares of each winner in the "3+1" tier.
 	 * @return An int representing the shares of each winner in the "3+1" tier.
 	 */
-	public int getPrizeTier3_1dividend() {return this.prizeTier3_1dividend;}
+	public double getPrizeTier3_1dividend() {return this.prizeTier3_1dividend;}
 
 	/**
 	 * Get the number of winners in the "3" tier.
@@ -258,7 +268,7 @@ public class JokerDrawData
 	 * Get the shares of each winner in the "3" tier.
 	 * @return An int representing the shares of each winner in the "3" tier.
 	 */
-	public int getPrizeTier3dividend() {return this.prizeTier3dividend;}
+	public double getPrizeTier3dividend() {return this.prizeTier3dividend;}
 
 	/**
 	 * Get the number of winners in the "2+1" tier.
@@ -270,7 +280,7 @@ public class JokerDrawData
 	 * Get the shares of each winner in the "2+1" tier.
 	 * @return An int representing the shares of each winner in the "2+1" tier.
 	 */
-	public int getPrizeTier2_1dividend() {return this.prizeTier2_1dividend;}
+	public double getPrizeTier2_1dividend() {return this.prizeTier2_1dividend;}
 
 	/**
 	 * Get the number of winners in the "1+1" tier.
@@ -282,5 +292,5 @@ public class JokerDrawData
 	 * Get the shares of each winner in the "1+1" tier.
 	 * @return An int representing the shares of each winner in the "1+1" tier.
 	 */
-	public int getPrizeTier1_1dividend() {return this.prizeTier1_1dividend;}
+	public double getPrizeTier1_1dividend() {return this.prizeTier1_1dividend;}
 }
